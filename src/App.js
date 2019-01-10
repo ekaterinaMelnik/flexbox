cf8b7cad85fad1b337fea5618fbd37f61fde9664
EkaterinaMelnik <melnykkatia@gmail.com>
2019-01-10 18:58:09 +0200

    create project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as styles from './App.module.scss';
+import Info from './Info/Info';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <div className={styles.container}>
+          <div className={styles.block1}>
+            Flexbox призван спасти нас от неприятных моментов чистого CSS (например, от вертикального выравнивания), и
+            он отлично справляется со своей задачей. Но разобраться в принципах его работы порой бывает сложно,
+            особенно, если вы новичок.
+          </div>
+          <div className={styles.block2}>
+            У нас есть 4 разноцветных div’а разных размеров, которые находятся внутри серого div’а. У каждого div’а есть
+            свойство display: block. Поэтому каждый квадрат занимает всю ширину строки.
+          </div>
+          <div className={styles.block3}>
+            Flexbox призван спасти нас
+          </div>
+        </div>
+        <div className={styles.tabContainer}>
+          <div className={styles.header}>
+            Header
+          </div>
+          <div className={styles.subheader}>
+            Subheader
+          </div>
+          <div className={styles.info}>
+            Info
+          </div>
+        </div>
+        <Info />
+      </>
     );
   }
 }
---
+++ src/Info/Info.jsx
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import * as styles from './Info.module.scss';
+
+const Info = () => (
+  <>
+    <div className={styles.root}>
+      <div className={styles.container}>
+      <div className={styles.header}>
+        Рівні доступу
+      </div>
+      <div className={styles.info}>
+        FINMAP дає можливість налаштувати різні рівні доступу під різні типи співробітників. Нехай бухгалтер бачить
+        тільки розрахунковий рахунок, а касир тільки деякі категорії доходів.
+      </div>
+    </div>
+      <div className={styles.container}>
+        <div className={styles.header}>
+          Рівні доступу
+        </div>
+        <div className={styles.info}>
+          FINMAP дає можливість налаштувати різні рівні доступу під різні типи співробітників. Нехай бухгалтер бачить
+          тільки розрахунковий рахунок, а касир тільки деякі категорії доходів.
+        </div>
+      </div>
+      <div className={styles.container}>
+        <div className={styles.header}>
+          Рівні доступу
+        </div>
+        <div className={styles.info}>
+          FINMAP дає можливість налаштувати різні рівні доступу під різні типи співробітників. Нехай бухгалтер бачить
+          тільки розрахунковий рахунок, а касир тільки деякі категорії доходів.
+        </div>
+      </div>
+      <div className={styles.container}>
+        <div className={styles.header}>
+          Рівні доступу
+        </div>
+        <div className={styles.info}>
+          FINMAP дає можливість налаштувати різні рівні доступу під різні типи співробітників. Нехай бухгалтер бачить
+          тільки розрахунковий рахунок, а касир тільки деякі категорії доходів.
+        </div>
+      </div>
+    </div>
+  </>
+);
+
+export default Info;
